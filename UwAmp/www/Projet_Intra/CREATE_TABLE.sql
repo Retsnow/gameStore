34c2c6640fe64e2417246488b8a3100024a356b1
Retsnow <jeremysoleil5@gmail.com>
2024-12-16 15:02:07 -0500

    sortir uwamp

--- UwAmp/www/Projet_Intra/CREATE_TABLE.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE emprunt (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usager_id INT,
-    periodique_id INT,
-    FOREIGN KEY (usager_id) REFERENCES usager(id),
-    FOREIGN KEY (periodique_id) REFERENCES periodique(id)
-);
